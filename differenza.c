025fff820e11e462a7339cdda12b249466e74e2c
Roland89 <rdland@gmail.com>
2018-12-14 22:38:24 +0100

    Creato Branch, creato file differenza.c all interno

---
+++ differenza.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=0, b=0;
+    printf("Inserisci il primo numero:");
+    scanf("%d",&a );
+    printf("Inserisci il secondo numero:");
+    scanf("%d",&b );
+    a=a-b;
+    printf("La differenza e':%d ", a);
+    return 0;
+}
